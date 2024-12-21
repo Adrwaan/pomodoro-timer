23ed4a319261920c6e173b912881d8225b09b34f
Adrwaan <adrean.danilo113@gmail.com>
2024-12-20 22:17:27 -0300

    Revert "style: adding image background"
    This reverts commit 6e3d4b9691d78c0fcfaefafd691c5abaf3d0374b.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-import { fontFamily } from "tailwindcss/defaultTheme";
-
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Montserrat", ...fontFamily.sans],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
