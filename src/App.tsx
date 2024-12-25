e98076434242dcc6849ed23061ec7432b2b6240d
Adrwaan <adrean.danilo113@gmail.com>
2024-12-24 22:48:49 -0300

    style(app.tsx): changed height breakpoint

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
     <ThemeProvider defaultTheme="dark" storageKey="ui-theme">
       <main
         id="main"
-        className="w-5/6 md:w-3/5 h-4/5 lg:h-3/5 backdrop-blur-lg rounded-lg flex flex-col items-center p-5 border border-white/20"
+        className="w-5/6 md:w-3/5 h-4/5 md:h-3/5 backdrop-blur-lg rounded-lg flex flex-col items-center p-5 border border-white/20"
       >
         <h1 className="text-2xl md:text-3xl font-semibold text-gray-200 border-b border-white/20 px-4 pb-1 text-center">
           Pomodoro Timer
